866709f3b38db4e605901bcddf6d0cf979708624
unknown <wellyngtonc._santos@hotmail.com>
2022-06-09 18:38:32 -0300

    update geral

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="img">
     <img src="../src/assets/pokemon-logo.png" alt="logo" />
+    {{modalDados}}
   </div>
   <div class="input-group mb-3 search">
     <input
@@ -29,32 +30,101 @@
   <div>
     <div class="card_content row g-3">
       <div style="width: 22rem" v-for="pokemon in filtered" :key="pokemon.name">
-        <div class="card" @click="show_pokemon">
+        <div class="card" @click="show_pokemon(pokemon.name)">
           <img
             :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
               pokemon
             )}.png`"
             :alt="pokemon.name"
-            class="PokeImg"
+            class="PokeImgCard"
           />
           <h2 class="card-title text-center">{{ get_name(pokemon) }}</h2>
         </div>
       </div>
     </div>
-    <m-dialog v-model="modalDados" v-if="selected_pokemon">
+    <!-- <paginate
+      v-model="page"
+      :page-count="totalPages"
+      :page-range="3"
+      :margin-pages="2"
+      :prev-text="'Voltar'"
+      :next-text="'Avançar'"
+      :container-class="'pagination'"
+      :page-class="'page-item'"
+    >
+    </paginate> -->
+    <m-dialog
+      class="modal_content"
+      v-model="modalDados"
+      v-if="selected_pokemon"
+    >
+      <h2 class="text-center">{{ get_name(selected_pokemon) }}</h2>
       <img
         :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`"
         :alt="selected_pokemon.name"
-        class="PokeImg"
+        class="PokeImgModal"
       />
-      <p>teste</p>
-      {{ selected_pokemon }}
+      <br />
+      <span>Types: </span>
+      <button
+        v-for="type in selected_pokemon.types"
+        :key="type.slot"
+        :type="type.type.name"
+        class="btn btn-primary pokeData"
+        disabled
+      >
+        {{ type.type.name }}
+      </button>
+
+      <br />
+      <span>Abilities: </span>
+      <button
+        v-for="ability in selected_pokemon.abilities"
+        :key="ability.slot"
+        :ability="ability.ability.name"
+        class="btn btn-primary pokeData"
+        disabled
+      >
+        {{ ability.ability.name }}
+      </button>
+
+      <br />
+      <span>Weight: </span>
+      <button class="btn btn-primary pokeData" disabled>
+        {{ (selected_pokemon.weight * 0.1).toFixed(1) }} Kg
+      </button>
+      <br />
+      <span>Height: </span>
+      <button class="btn btn-primary pokeData" disabled>
+        {{ (selected_pokemon.height * 0.1).toFixed(1) }} m
+      </button>
+      <br />
+      <span>Speed: </span>
+      <button class="btn btn-primary pokeData" disabled>
+        {{ selected_pokemon.stats.stat }}
+      </button>
+      <template  v-for="(evolution_detail, index) in evolutions()">
+         <div
+          :key="`evolution-${index}`"
+          v-if="typeof evolution_detail == 'object'"
+        >
+        <div :pokemon="evolution_detail"></div>
+        </div>
+        <div :key="`evolution-${index}`" v-else>
+          <h3 class="text-center">Level UP on {{ evolution_detail }}</h3>
+        </div>
+      </template >
     </m-dialog>
   </div>
 </template>
 
 <script>
+// import Paginate from "vuejs-paginate-next";
 import axios from "axios";
+
+// const page = ref(1);
+// const perPage = ref(5);
+
 export default {
   name: "App",
 
@@ -64,6 +134,7 @@ export default {
       search: "",
       modalDados: false,
       selected_pokemon: null,
+      evolution: null,
     };
   },
   mounted() {
@@ -79,12 +150,48 @@ export default {
       return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     },
     show_pokemon(name) {
-      console.log("teste");
-      console.log(this.selected_pokemon);
-      this.modalDados = true;
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
-        this.selected_pokemon = response.data;
-      });
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response) => {
+          this.selected_pokemon = response.data;
+          this.get_level()
+          this.modalDados = true;
+        });
+    },
+    get_level() {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon-species/${this.selected_pokemon.id}`)
+        .then((response) => {
+          axios.get(response.data.evolution_chain.url).then((response) => {
+            this.evolution = response.data.chain;
+            console.log(this.evolution.species.url)
+          });
+        });
+    }, 
+    evolutions() {
+      let chain = [];
+      let evolution = this.evolution;
+      console.log(this.evolution.species)
+      if (evolution.species) {
+        chain.push(evolution.species);
+      }
+      while (evolution.species) {
+        if (evolution.evolves_to.length > 0) {
+          evolution = evolution.evolves_to[0];
+          if (
+            evolution.evolution_details.length > 0 &&
+            evolution.evolution_details[0].min_level
+          ) {
+            chain.push(evolution.evolution_details[0].min_level);
+          }
+          if (evolution.species) {
+            chain.push(evolution.species);
+          }
+        } else {
+          break;
+        }
+      }
+      return chain;
     },
   },
   computed: {
@@ -98,6 +205,10 @@ export default {
 </script>
 
 <style>
+.pokeData {
+  margin-left: 10px;
+  margin-bottom: 10px;
+}
 .verMais {
   margin-top: 30px;
 }
@@ -117,8 +228,12 @@ export default {
   bottom: 0 !important;
   top: 15px;
 }
-.PokeImg {
+.PokeImgCard {
   width: 80%;
+}
+.PokeImgModal {
+  width: 50%;
+  flex-direction: column;
 }
 .card {
   width: 30vh;
@@ -130,8 +245,9 @@ export default {
   border-radius: 20px !important;
   align-items: center;
 }
-.card_content {
-  width: 100%;
+.modal_content {
+  display: flex;
+  text-align: center;
 }
 body {
   margin: 0;
